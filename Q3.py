a6c5d487eb959cf333922c02844615d039dd7552
태민 <jkgjms3@naver.com>
2022-07-29 16:01:34 +0900

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -3,6 +3,4 @@
 store = [2,3,7,8,9]
 customer = [7,5,9]
 
-target = list(set(store) & set(customer))
-
-['yes' if customer[i] in target else 'no' for i in range(len(customer))]
+['yes' if customer[i] in list(set(store) & set(customer)) else 'no' for i in range(len(customer))]
